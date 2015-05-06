5b1495914bff1e6aa8aa075fe08e3b53d79a6270
qct <quchentao@163.com>
2015-05-06 14:08:37 +0800

    f

--- src/main/java/thread/ThreadTest.java
+++ src/main/java/thread/ThreadTest.java
@@ -5,11 +5,17 @@ package thread;
  */
 public class ThreadTest {
     public static void main(String[] args) {
-        Count c = new Count();
-        Thread t1 = new Thread(c);
-        Thread t2 = new Thread(c);
-        t1.start();
-        t2.start();
+        CountThread b = new CountThread();
+        b.start();
+        synchronized (b) {
+            System.out.println("Waiting for b to complete...");
+            try {
+                b.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Total is: " + b.total);
+        }
     }
 }
 
