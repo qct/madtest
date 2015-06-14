88f47dc62fc3ef9961182abdf2f90fccc9c71f33
qct <quchentao@163.com>
2015-06-14 23:36:35 +0800

    f

---
+++ src/main/java/mongodb/MongoTest.java
@@ -0,0 +1,23 @@
+package mongodb;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.Iterator;
+
+/**
+ * Created by qct on 2015/6/11.
+ */
+public class MongoTest {
+    public static void main(String[] args) {
+        MongoClient client = MongoManager.getClient();
+        MongoDatabase database = client.getDatabase("test");
+        MongoCollection<Document> collection = database.getCollection("test");
+        collection.insertOne(new Document("sweeperTest", "hi"));
+
+        FindIterable<Document> documents = collection.find();
+    }
+}
