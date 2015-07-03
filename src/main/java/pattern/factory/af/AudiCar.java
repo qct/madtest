62945c1faad00f82d4591f1f9fc169ba19313378
qct <quchentao@163.com>
2015-07-03 17:10:54 +0800

    f

--- src/main/java/pattern/factory/af/AudiCar.java
+++ src/main/java/pattern/factory/af/AudiCar.java
@@ -1,8 +1,5 @@
 package pattern.factory.af;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 /**
  * Created by qct on 2015/6/4.
  */
