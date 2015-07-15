6ceb706ca18c5d321ca176e8fb440b7c40598120
qct <quchentao@163.com>
2015-07-15 10:52:44 +0800

    f

---
+++ madtest-common/src/main/java/madtest/common/util/Dom4jUtil.java
@@ -0,0 +1,61 @@
+package madtest.common.util;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URL;
+
+/**
+ * Created by qct on 2015/7/13.
+ */
+public class Dom4jUtil {
+
+    public static Document readXML(String fileName) {
+        SAXReader saxReader = new SAXReader();
+        Document document=null;
+        try {
+            URL xmlPath = Dom4jUtil.class.getClassLoader().getResource(fileName);
+            document = saxReader.read(xmlPath);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
+
+    public static Document readXML(InputStream in) {
+        SAXReader saxReader = new SAXReader();
+        Document document=null;
+        try {
+            document = saxReader.read(in);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
+
+    public static Document readXML(Reader reader) {
+        SAXReader saxReader = new SAXReader();
+        Document document=null;
+        try {
+            document = saxReader.read(reader);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
+
+    public static Document readXML(File file) {
+        SAXReader saxReader = new SAXReader();
+        Document document=null;
+        try {
+            document = saxReader.read(file);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
+}
