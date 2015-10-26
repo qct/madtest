f0b472b95975a59af787a514c06a27a345e4c1bb
quchentao <quchentao@163.com>
2015-10-26 23:14:56 +0800

    aio

---
+++ madtest-common/src/main/java/madtest/common/netty/study/chapter2/aio/TimeServer.java
@@ -0,0 +1,19 @@
+package madtest.common.netty.study.chapter2.aio;
+
+/**
+ * Created by quchentao on 15/10/26.
+ */
+public class TimeServer {
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                //
+            }
+        }
+        AsyncTimeServerHandler timeServer = new AsyncTimeServerHandler(port);
+        new Thread(timeServer, "AIO-AsyncTimeServerHandler-001").start();
+    }
+}
