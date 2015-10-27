e72a3c4e91013ab630be81ad1b429733e36388fd
quchentao <quchentao@163.com>
2015-10-27 22:49:37 +0800

    netty chapter5

---
+++ madtest-common/src/main/java/madtest/common/netty/study/chapter5/delimiter/EchoClientHandler.java
@@ -0,0 +1,39 @@
+package madtest.common.netty.study.chapter5.delimiter;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Created by quchentao on 15/10/27.
+ */
+public class EchoClientHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+
+    static final String ECHO_REQ = "Hi, Quchentao. Welcome to Netty.$_";
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for (int i=0;i<100;i++) {
+            ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("This is " + ++counter + " tiimes receive server: [" + msg + "]");
+    }
+}
