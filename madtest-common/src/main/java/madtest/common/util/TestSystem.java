9cdb33c58cd19421b185601456ae42b80ce823cc
quchentao-office <quchentao@163.com>
2016-07-13 14:24:19 +0800

    env

---
+++ madtest-common/src/main/java/madtest/common/util/TestSystem.java
@@ -0,0 +1,10 @@
+package madtest.common.util;
+
+/**
+ * Created by alex on 2016/7/13.
+ */
+public class TestSystem {
+    public static void main(String[] args) {
+        System.out.println(System.getenv("MYSQL_HOST"));
+    }
+}
