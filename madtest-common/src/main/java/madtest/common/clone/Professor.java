cf8182f9112bcd67010bf7d6ff2946028b2342be
quchentao-office <quchentao@163.com>
2016-10-19 13:30:08 +0800

    kubernetes examples

---
+++ madtest-common/src/main/java/madtest/common/clone/Professor.java
@@ -0,0 +1,24 @@
+package madtest.common.clone;
+
+/**
+ * Created by alex on 2016/10/18.
+ */
+public class Professor implements Cloneable{
+    String name;
+    int age;
+    Professor(String name,int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+
+    public Object clone() {
+        Object o = null;
+        try {
+            o = super.clone();//Object 中的clone()识别出你要复制的是哪一个对象。
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e.toString());
+        }
+        return o;
+    }
+}
---
+++ madtest-common/src/main/java/madtest/common/clone/Student2.java
@@ -0,0 +1,36 @@
+package madtest.common.clone;
+
+/**
+ * Created by alex on 2016/10/18.
+ */
+public class Student2 implements Cloneable {
+    String name;// 常量对象。
+    int age;
+    Professor p;// 学生1和学生2的引用值都是一样的。
+
+    Student2(String name, int age, Professor p) {
+        this.name = name;
+        this.age = age;
+        this.p = p;
+    }
+
+    public Object clone() {
+        Student2 o = null;
+        try {
+            o = (Student2) super.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e.toString());
+        }
+        o.p = (Professor) p.clone();
+        return o;
+    }
+
+    public static void main(String[] args) {
+        Professor p = new Professor("wangwu", 50);
+        Student2 s1 = new Student2("zhangsan", 18, p);
+        Student2 s2 = (Student2) s1.clone();
+        s2.p.name = "lisi";
+        s2.p.age = 30;
+        System.out.println("name=" + s1.p.name + "," + "age=" + s1.p.age);//学生1的教授成为lisi,age为30。
+    }
+}
