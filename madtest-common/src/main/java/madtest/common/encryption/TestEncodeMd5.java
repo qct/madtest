9cdac1ca5578e8f8d22d5d833b42a8ca238499de
alex <tango.qct@gmail.com>
2017-01-18 14:14:04 +0800

    rsa

--- madtest-common/src/main/java/madtest/common/encryption/TestEncodeMd5.java
+++ madtest-common/src/main/java/madtest/common/encryption/TestEncodeMd5.java
@@ -2,7 +2,11 @@ package madtest.common.encryption;
 
 import com.google.common.collect.Lists;
 
+
 import org.apache.commons.codec.EncoderException;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.digest.UnixCrypt;
+import org.apache.commons.codec.net.URLCodec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,12 +20,12 @@ public class TestEncodeMd5 {
     static Logger logger = LoggerFactory.getLogger(TestEncodeMd5.class);
 
     public static void main(String[] args) throws EncoderException {
-/*        String pwd = "123";
+        String pwd = "123";
         logger.debug(org.springframework.util.DigestUtils.md5DigestAsHex(pwd.getBytes()));
         logger.debug(DigestUtils.md5Hex(pwd.getBytes()));
         logger.debug(UnixCrypt.crypt(pwd.getBytes()));//DES
         URLCodec urlCodec = new URLCodec();
-        logger.debug(urlCodec.encode("http://www.baidu.com"));*/
+        logger.debug(urlCodec.encode("http://www.baidu.com"));
 
         /*System.out.println(
         new String(Base64.getDecoder().decode
