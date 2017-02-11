133584b9ba8f4e09728238da72e5c996dcf9ed47
alex <tango.qct@gmail.com>
2017-02-11 13:29:53 +0800

    freemarker & thymeleaf

---
+++ madtest-freemarker/src/main/java/madtest/freemarker/IndexController.java
@@ -0,0 +1,17 @@
+package madtest.freemarker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * <p>Created by Damon.Q on 2017/2/11.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
---
+++ madtest-thymeleaf/src/main/java/madtest/thymeleaf/HelloController.java
@@ -0,0 +1,22 @@
+package madtest.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * <p>Created by Damon.Q on 2017/2/11.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(
+        @RequestParam(value = "name", required = false, defaultValue = "World") String name,
+        Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
