b3a5a9c3cf911ab25905e600359e7e4a4757d13e
qct <quchentao@163.com>
2017-05-13 06:31:13 +0000

    update

---
+++ madtest-common/src/main/java/madtest/common/queue/PeekPollTest.java
@@ -0,0 +1,41 @@
+package madtest.common.queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentMap;
+import org.eclipse.jetty.util.ConcurrentArrayQueue;
+
+/**
+ * <p>Created by Damon.Q on 2017/5/6.
+ */
+public class PeekPollTest {
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayDeque<>(10);
+
+        new Thread(() -> {
+            while (true) {
+                Integer peek = queue.peek();
+                if(peek == null) {
+                    for (int i=0;i<20;i++) {
+                        boolean offer = queue.offer(i);
+                        System.out.println("insert successfully, " + i);
+                    }
+                }else {
+                    Integer poll = queue.poll();
+                    if(poll != null) {
+                        System.out.println("do something... : " + poll);
+                    }else {
+                        System.out.println("queue is empty, waiting for insert...");
+                    }
+                }
+
+                try {
+                    Thread.sleep(1000l);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
---
+++ madtest-common/src/main/java/madtest/common/spring/lifecycle/MyLifecycle.java
@@ -0,0 +1,47 @@
+package madtest.common.spring.lifecycle;
+
+import org.springframework.context.SmartLifecycle;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>Created by Damon.Q on 2017/4/27.
+ */
+@Component
+public class MyLifecycle implements SmartLifecycle {
+
+    private boolean isRunning = false;
+
+    @Override
+    public boolean isAutoStartup() {
+        System.out.println("check isAutoStartup...");
+        return true;
+    }
+
+    @Override
+    public void stop(Runnable runnable) {
+        System.out.println("I'm stopping");
+        runnable.run();
+    }
+
+    @Override
+    public void start() {
+        System.out.println("I'm starting");
+        isRunning = true;
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("I'm stopping");
+    }
+
+    @Override
+    public boolean isRunning() {
+        System.out.println("check isRunning: " + isRunning);
+        return isRunning;
+    }
+
+    @Override
+    public int getPhase() {
+        return 10;
+    }
+}
